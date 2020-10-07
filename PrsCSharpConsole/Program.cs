f571d8b2d3d70bc15ee9ab274a1e717c598402e4
Greg Doud <gpdoud@gmail.com>
2020-10-07 16:41:16 -0400

    Finished User class and controller

--- PrsCSharpConsole/Program.cs
+++ PrsCSharpConsole/Program.cs
@@ -1,5 +1,6 @@
 ﻿using PrsDbLib;
 using PrsDbLib.Controllers;
+using PrsDbLib.Models;
 
 using System;
 
@@ -10,8 +11,22 @@ namespace PrsCSharpConsole {
             var prsconn = new PrsConnection("localhost", "PRS0");
             prsconn.Connect();
 
+            var user = new User {
+                Id = 0, Username = "zz", Password = "zz", Firstname = "zz", Lastname = "zz",
+                Phone = "zz", Email = "zz", IsReviewer = true, IsAdmin = true
+            };
+
             var usersCtrl = new UsersController(prsconn);
+
+            //var recsAffected = usersCtrl.Insert(user);
+
             var users = usersCtrl.GetUsers();
+
+            var user1 = usersCtrl.GetUser(9);
+            user1.Firstname = "Noah";
+            user1.Lastname = "Phence";
+
+            var recsAffected = usersCtrl.Update(user1);
 
             prsconn.Disconnect();
         }
--- PrsDbLib/Controllers/UsersController.cs
+++ PrsDbLib/Controllers/UsersController.cs
@@ -16,6 +16,73 @@ namespace PrsDbLib.Controllers {
             this.prsConnection = prsConnection;
         }
 
+        public int Update(User user) {
+            var sql = "UPDATE Users SET " +
+                " Username = @Username, " +
+                " Password = @Password, " +
+                " Firstname = @Firstname, " +
+                " Lastname = @Lastname, " +
+                " PhoneNumber = @Phone, " +
+                " Email = @Email, " +
+                " IsReviewer = @IsReviewer, " +
+                " IsAdmin = @IsAdmin " +
+                " WHERE Id = @Id;";
+            var cmd = new SqlCommand(sql, prsConnection.sqlConnection);
+            cmd.Parameters.AddWithValue("@Username", user.Username);
+            cmd.Parameters.AddWithValue("@Password", user.Password);
+            cmd.Parameters.AddWithValue("@Firstname", user.Firstname);
+            cmd.Parameters.AddWithValue("@Lastname", user.Lastname);
+            cmd.Parameters.AddWithValue("@Phone", user.Phone);
+            cmd.Parameters.AddWithValue("@Email", user.Email);
+            cmd.Parameters.AddWithValue("@IsReviewer", user.IsReviewer);
+            cmd.Parameters.AddWithValue("@IsAdmin", user.IsAdmin);
+            cmd.Parameters.AddWithValue("@Id", user.Id);
+            return cmd.ExecuteNonQuery();
+        }
+
+        public int Insert(User user) {
+            var sql = "INSERT Users (Username, Password, Firstname, Lastname, PhoneNumber, Email, IsReviewer, IsAdmin)" +
+                       " VALUES (@Username, @Password, @Firstname, @Lastname, @Phone, @Email, @IsReviewer, @IsAdmin);";
+            var cmd = new SqlCommand(sql, prsConnection.sqlConnection);
+            cmd.Parameters.AddWithValue("@Username", user.Username);
+            cmd.Parameters.AddWithValue("@Password", user.Password);
+            cmd.Parameters.AddWithValue("@Firstname", user.Firstname);
+            cmd.Parameters.AddWithValue("@Lastname", user.Lastname);
+            cmd.Parameters.AddWithValue("@Phone", user.Phone);
+            cmd.Parameters.AddWithValue("@Email", user.Email);
+            cmd.Parameters.AddWithValue("@IsReviewer", user.IsReviewer);
+            cmd.Parameters.AddWithValue("@IsAdmin", user.IsAdmin);
+            return cmd.ExecuteNonQuery();
+        }
+
+        private void DataReaderToUserInstance(User user, SqlDataReader sqlDataReader) {
+            user.Id = Convert.ToInt32(sqlDataReader["Id"]);
+            user.Username = Convert.ToString(sqlDataReader["Username"]);
+            user.Password = Convert.ToString(sqlDataReader["Password"]);
+            user.Firstname = Convert.ToString(sqlDataReader["Firstname"]);
+            user.Lastname = Convert.ToString(sqlDataReader["Lastname"]);
+            user.Phone = Convert.ToString(sqlDataReader["PhoneNumber"]);
+            user.Email = Convert.ToString(sqlDataReader["Email"]);
+            user.IsReviewer = Convert.ToBoolean(sqlDataReader["IsReviewer"]);
+            user.IsAdmin = Convert.ToBoolean(sqlDataReader["IsAdmin"]);
+        }
+
+        public User GetUser(int Id) {
+            var sql = "SELECT * From Users Where Id = @Id;";
+            var cmd = new SqlCommand(sql, prsConnection.sqlConnection);
+            cmd.Parameters.AddWithValue("@Id", Id);
+            var sqlDataReader = cmd.ExecuteReader();
+            if(!sqlDataReader.HasRows)
+                return null;
+            sqlDataReader.Read();
+            var user = new User();
+            // duplicate lines pulled from here
+            DataReaderToUserInstance(user, sqlDataReader);
+
+            sqlDataReader.Close();
+            return user;
+        }
+
         public List<User> GetUsers() {
             var sql = "SELECT * From Users;";
             var cmd = new SqlCommand(sql, prsConnection.sqlConnection);
@@ -23,16 +90,7 @@ namespace PrsDbLib.Controllers {
             var sqlDataReader = cmd.ExecuteReader();
             while(sqlDataReader.Read()) {
                 var user = new User();
-                
-                user.Id = Convert.ToInt32(sqlDataReader["Id"]);
-                user.Username = Convert.ToString(sqlDataReader["Username"]);
-                user.Password = Convert.ToString(sqlDataReader["Password"]);
-                user.Firstname = Convert.ToString(sqlDataReader["Firstname"]);
-                user.Lastname = Convert.ToString(sqlDataReader["Lastname"]);
-                user.Phone = Convert.ToString(sqlDataReader["PhoneNumber"]);
-                user.Email = Convert.ToString(sqlDataReader["Email"]);
-                user.IsReviewer = Convert.ToBoolean(sqlDataReader["IsReviewer"]);
-                user.IsAdmin = Convert.ToBoolean(sqlDataReader["IsAdmin"]);
+                DataReaderToUserInstance(user, sqlDataReader);
                 
                 users.Add(user);
             }
